c5675b0881399dc56c64cb820ee0dea002e94747
Ignacio <nachov663@gmail.com>
2020-09-26 03:07:07 -0300

    26/09/2020

--- app/src/main/java/app/iggy/ites/BaseActivity.java
+++ app/src/main/java/app/iggy/ites/BaseActivity.java
@@ -10,7 +10,6 @@ public class BaseActivity extends AppCompatActivity {
     private static final String TAG = "BaseActivity";
     static final String PHOTO_TRANSFER = "PHOTO_TRANSFER";
 
-    //  we pass true or false from the classes that are extended from this class to activate or deactivate the toolbar button
     void activateToolbar(boolean enableHome){
         Log.d(TAG, "activateToolbar: starts");
         ActionBar actionBar = getSupportActionBar();
